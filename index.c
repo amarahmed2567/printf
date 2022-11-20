22a2bd49e563a49f502dc310966402bf5956a4fa
Amar Ahmed <amarahmed2567@gmail.com>
2022-11-21 00:12:44 +0200

    Added printf fun

---
+++ index.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/*
+    If States 0 Then it will be reguler
+    If States 1 Then it will be escape
+*/
+void printfHandel(const char *format, va_list args)
+{
+    int state = 0;
+    while (*format)
+    {
+        if (state == 0)
+        {
+            if (*format == '%')
+            {
+                state = 1;
+            }
+            else
+            {
+                putchar(*format);
+            }
+        }
+        else if (state == 1)
+        {
+            switch (*format)
+            {
+            case 'c':
+            {
+                putchar(va_arg(args, int));
+                break;
+            }
+            case 's':
+            {
+                const char *s = va_arg(args, const char *);
+                while (*s)
+                {
+                    putchar(*s++);
+                }
+                break;
+            }
+            }
+            state = 0;
+        }
+        format++;
+    }
+}
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args,format);
+    printfHandel(format,args);
+    va_end(args);
+}
+
+int main()
+{
+    char str1[] = "Hello";
+    char str2[] = "World!";
+    _printf("Character is: %c Done! \n", 'A');
+    _printf("String is: %s %s \n", str1, str2);
+    return 0;
+}
